e375f77a9b3d7e12a148d85a86c23c015e00fc7c
saapad86 <saad.padela@gmail.com>
2015-12-12 18:02:29 -0500

    hive loading
    changed file separator from ; to \t
    this should load hive tables.

--- api/census.py
+++ api/census.py
@@ -34,11 +34,11 @@ def fetch_census_data(params,series,seriesDesc):
 
 def create_census_datafile(data,seriesDesc,fileNum):       
     filename = "census"+str(fileNum)+".txt"
-    target = codecs.open('/home/w205/hive/census_data'+filename,'w','UTF-8')
+    target = codecs.open('/home/w205/hive/census_data/'+filename,'w','UTF-8')
     for row in data:
         county_txt = row[0].encode('ascii','replace').split(",")
         cols = [county_txt[0],county_txt[1],seriesDesc,row[1]]
-        target.write(';'.join(cols)+'\n')
+        target.write('\t'.join(cols)+'\n')
 
 seriesList = ["NAME,DP03_0037E","NAME,DP03_0039E","NAME,DP03_0040E","NAME,DP03_0041E","NAME,DP03_0042E","NAME,DP03_0045E","NAME,DP04_0100E","NAME,DP04_0132E","NAME,DP05_0001E","NAME,DP05_0008E","NAME,DP05_0009E"]
 seriesDescList = ["jobs_retail","jobs_it","jobs_finance","job_research","jobs_education","jobs_public","housing_cost_own","housing_cost_rent","pop_tot","pop_20_24","pop_25_34"]
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,4 +3,6 @@ mkdir /home/w205/hive/census_data
 cd /home/w205/hive/census_data
 su w205
 hdfs dfs -mkdir /user/w205/census_data/
+python /root/api/census.py
 hdfs dfs -put /home/w205/hive/census_data/* /user/w205/census_data
+hive -f /root/hive/census.sql
