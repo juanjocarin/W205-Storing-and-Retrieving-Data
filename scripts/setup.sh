b24c097e00f70601e57e95f7f6a8eb090bacf11b
saapad86 <saad.padela@gmail.com>
2015-12-12 18:16:15 -0500

    working hive table

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
 mkdir /home/w205/hive
 mkdir /home/w205/hive/census_data
+python /root/api/census.py
 cd /home/w205/hive/census_data
 su w205
 hdfs dfs -mkdir /user/w205/census_data/
-python /root/api/census.py
+hdfs dfs -rmr /user/w205/census_data/*
 hdfs dfs -put /home/w205/hive/census_data/* /user/w205/census_data
+exit
 hive -f /root/hive/census.sql
