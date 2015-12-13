5b9d2999dcb99f891cde8c012e3aef91b1d01cb6
Juanjo Carin <juanjose.carin@ischool.berkeley.edu>
2015-12-12 16:46:43 -0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,14 @@
-mkdir /home/w205/hive
-mkdir /home/w205/hive/census_data
-python /root/api/census.py
-cd /home/w205/hive/census_data
-su w205
-hdfs dfs -mkdir /user/w205/census_data/
-hdfs dfs -rmr /user/w205/census_data/*
-hdfs dfs -put /home/w205/hive/census_data/* /user/w205/census_data
-exit
-hive -f /root/hive/census.sql
+# Run Python script to get data from Census API
+python ../api/census.py
+
+# Upload data to HDFS
+cd /data/w205/hive/
+hdfs dfs -mkdir /user/root/census_data/
+hdfs dfs -rm -r /user/root/census_data/*
+hdfs dfs -put /data/w205/hive/census_data/* /user/root/census_data
+
+# Create the DB
+hive -f /data/w205/W205_final/hive/census.sql
+
+# Check that the new DB has been created
+hive -S -e 'select * from census limit 10'
